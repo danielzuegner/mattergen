fd7bc6b5c0751e04d4decdc0e840eac105616907
Daniel Zuegner <daniel.zuegner@gmail.com>
2025-01-22 11:15:59 +0100

    add globals

--- mattergen/common/utils/globals.py
+++ mattergen/common/utils/globals.py
@@ -5,10 +5,29 @@
 1. It sets the environment variable `PROJECT_ROOT` to the root of the explorers project.
 2. It registers a new resolver for OmegaConf, `eval`, which allows us to use `eval` in our config files.
 """
+from functools import lru_cache
 import os
 from pathlib import Path
 
 from omegaconf import OmegaConf
+import torch
+
+@lru_cache
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    if torch.backends.mps.is_available():
+        return torch.device("mps")
+    return torch.device("cpu")
+
+@lru_cache
+def get_pyg_device():
+    """
+    Some operations of pyg don't work on MPS, so fall back to CPU.
+    """
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    return torch.device("cpu")
 
 MODELS_PROJECT_ROOT = Path(__file__).resolve().parents[2]
 print(f"MODELS_PROJECT_ROOT: {MODELS_PROJECT_ROOT}")
